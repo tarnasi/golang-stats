93ce520873379534e6646c2aa1a1bdfd019d97d8
tarnasi <shahryar.tarnasi@gmail.com>
2026-09-03 00:07:11 +0400

    started new challange

--- challenges/download-time-estimator/main.go
+++ challenges/download-time-estimator/main.go
@@ -1,11 +1,33 @@
 package main
 
+import (
+	"fmt"
+)
 
+
+func calculateDownloadTime(fileSizeMB float64, speedMBps float64) float64 {
+
+}
+
+func classifyDownload(seconds float64) string {
+
+}
 
 
 
 func main() {
+	var fileSize float64
+	var internetSpeed float64
+
+	fmt.Print("File Size in MB: ")
+	fmt.Scan(&fileSize)
+
+	fmt.Print("Internet speed: ")
+	fmt.Scan(internetSpeed)
+
 	
+
+
 }
 
 
